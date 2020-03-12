9ec60605d82edb65a22051e6d95fcf50c7a6bb85
muthu raveendran <muthuraveendran01@gmail.com>
2020-03-12 11:43:28 -0700

    Intial Commit with How to get id from Html File

---
+++ home.js
@@ -0,0 +1,11 @@
+// this is how to get element from html 
+document.getElementById("someText").innerHTML="Welecomr To Our Website"
+
+// this is how we need to store a value from prompt
+
+var age = prompt("what is your Age ?")
+// console.log(age);
+
+//how to store value to html content
+
+document.getElementById("someText").innerHTML=age;
